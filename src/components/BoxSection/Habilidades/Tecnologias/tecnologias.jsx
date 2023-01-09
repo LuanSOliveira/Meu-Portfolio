fb702b738722b8618c9be48167aac409bb70e1dd
Luan <l.u.a.n@hotmail.com>
2023-01-09 10:59:09 -0300

    atualização de responsividade

--- src/components/BoxSection/Habilidades/Tecnologias/tecnologias.jsx
+++ src/components/BoxSection/Habilidades/Tecnologias/tecnologias.jsx
@@ -2,8 +2,9 @@ import styles from './Tecnologias.module.scss'
 
 const Tecnologias = ({nome, imagem, delay}) => {
     let larguraTela = window.innerWidth
+    let alturaTela = window.innerHeight
     return (
-        <div className={styles.Tecnologias} data-aos={(larguraTela >= 1280) ? "flip-left" : ""} data-aos-delay={delay}>
+        <div className={styles.Tecnologias} data-aos={(larguraTela >= 1280 && alturaTela >= 650) ? "flip-left" : ""} data-aos-delay={delay}>
             <p>{nome}</p>
             <div className={styles.BoxImagem}>
                 <img src={imagem} alt={`ícone ${nome}`}/>
